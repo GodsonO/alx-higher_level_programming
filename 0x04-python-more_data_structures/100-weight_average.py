a0ad8d2eae53e84b952c41b68c9c9ad3ad8cff93
GodsonO <godsonomoghene15@gmail.com>
2023-06-14 17:15:41 -0700

    python

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,7 +6,7 @@ def weight_average(my_list=[]):
         _number = 0
         denominator = 0
         for tup in my_list:
-            number += (tup[0] * tup[1])
+            _number += (tup[0] * tup[1])
             denominator += tup[1]
-        return (number / denominator)
+        return (_number / denominator)
     return 0
