0cc92b194d9997a4e7f000531ba19c6d0c64c023
GodsonO <godsonomoghene15@gmail.com>
2023-07-11 16:14:57 -0700

    python

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
 """
-reads stdin line by line and computes metrics
+a script that reads stdin line by line and computes metrics
 """
 import sys
 
 file_size = 0
-status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+stat_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
+num = 0
 try:
     for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
+        token = line.split()
+        if len(token) >= 2:
+            a = num
+            if token[-2] in stat_tally:
+                stat_tally[token[-2]] += 1
+                num += 1
             try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
+                file_size += int(token[-1])
+                if a == num:
+                    num += 1
             except FileNotFoundError:
-                if a == i:
+                if a == num:
                     continue
-        if i % 10 == 0:
+        if num % 10 == 0:
             print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
+            for key, value in sorted(stat_tally.items()):
                 if value:
                     print("{:s}: {:d}".format(key, value))
     print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
+    for key, value in sorted(stat_tally.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
 
 except KeyboardInterrupt:
     print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
+    for key, value in sorted(stat_tally.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
