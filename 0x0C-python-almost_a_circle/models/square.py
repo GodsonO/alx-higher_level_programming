53a01a8c5f8491b887157f0f5202673915f46642
GodsonO <godsonomoghene15@gmail.com>
2023-07-21 12:20:25 -0700

    python

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
+# square.py
 """
-This defines a class Square that inherits from Rectangle
+Defines a class Square that inherits from Rectangle
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
-    Represents a square.
+    Represents a square using Rectangle.
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square"""
+        """Constructor."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Returns the print() and str() representation of this square."""
+        """Returns string info about this square."""
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """return size of this square."""
+        """Size of this square."""
         return self.width
 
     @size.setter
@@ -30,28 +31,32 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """ Updates attributes of the Square"""
+        """ Updates attributes of the Square
+        Args:
+            args (non-keyword arguments): non-specified amount of arguments
+            kwargs (key-word arguments): non-specified amount of arguments
+        """
 
-        D_attributes = ["id", "size", "x", "y"]
+        attrs = ["id", "size", "x", "y"]
 
-        for position, val in enumerate(args):
-            if position < len(D_attributes):
-                setattr(self, D_attributes[position], val)
+        for pos, val in enumerate(args):
+            if pos < len(attrs):
+                setattr(self, attrs[pos], val)
 
         if len(args) == 0:
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
 
-     def to_dictionary(self):
+    def to_dictionary(self):
         """
-        Returns the dictionary representation of the square
+        Returns the dictionary representation of the instance
         """
 
-        D_dictionary = {}
-        D_attributes = ["id", "size", "x", "y"]
+        new_dict = {}
+        attrs = ["id", "size", "x", "y"]
 
-        for i in attributes:
-            D_dictionary[i] = getattr(self, i)
+        for att in attrs:
+            new_dict[att] = getattr(self, att)
 
-        return D_dictionary
+        return new_dict
