d8d9cab5d707f5bb3d52867fee520129cac5af2f
GodsonO <godsonomoghene15@gmail.com>
2023-07-21 15:20:47 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,7 @@
 import json
 import csv
 import turtle
-
+import os
 
 class Base:
     """Represent the base model."""
@@ -18,12 +18,12 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
- @staticmethod
+  @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts.
-        Args:
-            list_dictionaries (list): A list of dictionaries.
-        """
+        """returns the JSON representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
+        if (type(list_dictionaries) != list or not
+                all(type(i) == dict for i in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
