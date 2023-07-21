75c0676c67f65fac724c58290f333f6016299c95
GodsonO <godsonomoghene15@gmail.com>
2023-07-21 15:31:55 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,23 +16,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-   @classmethod
-    def save_to_file(cls, list_objs):
-        """
-        Writes the JSON string representation of list_objs to a file
-        Args:
-            list_objs (list of objects): list of instances who inherits of Base
-        """
-
-        filename = "{}.json".format(cls.__name__)
-        my_list = []
-
-        if list_objs is not None:
-            for each in list_objs:
-                my_list.append(each.to_dictionary())
-
-        json_list = cls.to_json_string(my_list)
-
-        with open(filename, "w+", encoding="UTF-8") as f:
-            f.write(json_list)
