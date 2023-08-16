c33901fe49dbb6949f3cf0bd4b1740c9ac8084f7
GodsonO <godsonomoghene15@gmail.com>
2023-08-15 17:34:31 -0700

    sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,2 @@
--- updates teh score of Bob to 10 int the 
--- second table
--- using only the name field
+-- A script that updates the score of Bob to 10 in the second table
 UPDATE second_table SET score = 10 WHERE name = 'Bob';
