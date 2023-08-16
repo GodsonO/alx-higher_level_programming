7b5c1746e48a35ddbad67da11de01f7cb27ad4bf
GodsonO <godsonomoghene15@gmail.com>
2023-08-16 16:16:08 -0700

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
--- Creates the table hbtn_0d_usa with table states.
+-- A script that Creates the database table hbtn_0d_usa with table states.
+-- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     PRIMARY KEY(`id`),
