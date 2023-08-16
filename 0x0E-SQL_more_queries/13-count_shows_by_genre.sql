0596d76608aec0502ec0548cd8dde018fc0c6e03
GodsonO <godsonomoghene15@gmail.com>
2023-08-16 16:41:27 -0700

    sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
--- Lists all genres from the database hbtn_0d_tvshows along with the number of
+-- A script that Lists all genres from the database hbtn_0d_tvshows 
+-- along with the number of
 -- shows linked to each.
 -- Does not display genres without linked shows.
 -- Records are ordered by descending number of shows linked.
