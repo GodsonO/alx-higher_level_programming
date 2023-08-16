21c71241ffd81df190a13e8d5e78c4e9814b7b63
GodsonO <godsonomoghene15@gmail.com>
2023-08-15 17:24:46 -0700

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
--- create a table second_table in hbtn_0c_0
--- add multiple rows
+-- A script that create a table second_table in the database hbtn_0c_0
+-- in your MySQL server and add multiple rows
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
 score INT);
