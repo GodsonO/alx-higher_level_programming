ce33adcc58831acaf2cda1d8958b5986d2725af1
GodsonO <godsonomoghene15@gmail.com>
2023-09-12 00:15:48 +0000

    java

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
 const argument = process.argv.length;
 
-if (argument > 2) {
-  console.log('Argument' + ' found');
-} else {
-  console.log('No argument');
+if (argument === 3)
+{
+	console.log('Argument' + ' found');  
+}
+else if (argument > 3)
+{
+        console.log('Arguments found');
+}
+else 
+{
+	console.log('No argument');
 }
