0cef59dbce8c5fe16968bedcc443f9ded258996f
GodsonO <godsonomoghene15@gmail.com>
2023-09-17 13:47:15 -0700

    python

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,10 +2,6 @@
 """
 Lists the State object with the name passed as argument
 from the database hbtn_0e_6_usa.
-Usage: ./10-model_state_my_get.py <mysql username> /
-                                  <mysql password> /
-                                  <database name>
-                                  <state name searched>
 """
 import sys
 from sqlalchemy import create_engine
@@ -19,11 +15,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    found = False
+    f = False
     for state in session.query(State):
         if state.name == sys.argv[4]:
             print("{}".format(state.id))
-            found = True
+            f = True
             break
-    if found is False:
+    if f is False:
         print("Not found")
