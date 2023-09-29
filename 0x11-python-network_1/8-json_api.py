4c43453d81ab5b1f98b8bc1a76be41afe6e78245
GodsonO <godsonomoghene15@gmail.com>
2023-09-29 23:19:22 +0000

    net

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    D_letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    D_payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=D_payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
